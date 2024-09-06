eff172d47615a3cb567e03d36329a2c24c13c200
ChickenWhisky <thomasmampilli2003@gmail.com>
2024-09-06 12:15:55 +0530

    Created a window that can be closed by simply pressing the X button on the border

--- chip8.c
+++ chip8.c
@@ -1,17 +1,17 @@
 // SDL Libraries
-#include <SDL2/SDL_error.h>
-#include <SDL2/SDL_log.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_video.h>
-#include <SDL2/SDL_audio.h>
+
+// #include <SDL.h>
+#include <SDL2/SDL.h>
+
 
 // Standard C Libraries
-#include <cinttypes>
+#include <SDL2/SDL_events.h>
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h>
 
+// SDL Container Object
 typedef struct{
 	SDL_Window *window;
 	SDL_Renderer *renderer;
@@ -23,8 +23,18 @@ typedef struct{
 	uint32_t window_width;		// Width of the window
 	uint32_t fg_color;			// Foreground color rgba888
 	uint32_t bg_color;			// Background color rgba888
+	uint32_t scale;				// Scale of the window
 }config_t ;
 
+typedef enum{
+	QUIT,
+	RUNNING,
+	PAUSED,
+}emulator_state_t ;
+
+typedef struct{
+	emulator_state_t state;
+}chip8_t;
 
 // Initialize SDL
 bool init_sdl(sdl_t *sdl,const config_t config){
@@ -39,8 +49,8 @@ bool init_sdl(sdl_t *sdl,const config_t config){
 	sdl->window = SDL_CreateWindow("Chip8 Emulator",
 								SDL_WINDOWPOS_CENTERED,
 								SDL_WINDOWPOS_CENTERED,
-								config.window_width,
-							  	config.window_height,
+								config.window_width * config.scale,
+							  	config.window_height * config.scale,
 								SDL_WINDOW_SHOWN);
 	
 	// Check if window was created successfully
@@ -61,6 +71,12 @@ bool init_sdl(sdl_t *sdl,const config_t config){
 	return true; 
 }
 
+// Initialize Chip8
+bool init_chip8(chip8_t *chip8){
+	chip8->state = RUNNING;   // This is the default chip8 state
+	return true;
+}
+
 bool set_config_from_args(config_t *config,int argc,char **argv){
 	(void) argc;
 	(void) argv;
@@ -68,7 +84,9 @@ bool set_config_from_args(config_t *config,int argc,char **argv){
 	// Set the default values
 	config->window_height = 32;   //OG Chip8 Y resolution
 	config->window_width = 64;	  //OG Chip8 X resolution
-	
+	config->fg_color = 0xFFFFFFFF; // White
+	config->bg_color = 0xFFFF00FF; // Yellow
+	config->scale = 20;			  // Scale of the window
 	// Check if the user has provided the values
 	for(int i = 1;i<argc;i++){
 		if(strcmp(argv[i],"-h")==0){
@@ -96,6 +114,41 @@ void clear_screen(const sdl_t sdl,const config_t config){
 							(config.bg_color>>0)&0xFF);		// Get the alpha component
 	SDL_RenderClear(sdl.renderer);
 }
+void update_screen(const sdl_t sdl){
+	SDL_RenderPresent(sdl.renderer);
+}
+
+void handle_inputs(chip8_t *chip8){
+	SDL_Event event;
+	while(SDL_PollEvent(&event)){
+		switch(event.type){
+			case SDL_QUIT:				// Exits window and ends the program
+				chip8->state = QUIT;	
+				exit(EXIT_SUCCESS);
+				break;
+			case SDL_KEYDOWN:
+			case SDL_KEYUP:
+				switch(event.key.keysym.sym){
+					case SDLK_ESCAPE:
+						chip8->state = QUIT;
+						break;
+					case SDLK_SPACE:
+						if(chip8->state == RUNNING){
+							chip8->state = PAUSED;
+						}else{
+							chip8->state = RUNNING;
+						}
+						break;
+					default:
+						break;
+				}
+				break;
+			default:
+				break;
+		}
+	}
+}
+
 int main(int argc, char **argv){
 	(void) argc;
 	(void) argv;
@@ -108,18 +161,27 @@ int main(int argc, char **argv){
 	sdl_t sdl = {0};
 	if(!init_sdl(&sdl,config))exit(EXIT_FAILURE);
 	
+	// Initialize Chip8
+	chip8_t chip8 = {0};
+	if(!init_chip8(&chip8))exit(EXIT_FAILURE);
+	
 	// Initial screen clear
-	
-	SDL_SetRendererDrawColor();
-	SDL_RenderClear(sdl.renderer);
-	
+	clear_screen(sdl,config);
+
 	// Main Emulator loop
 	
-	while(true){
+	while(chip8.state!=QUIT){
+		
+		// Handle SDL Events
+		handle_inputs(&chip8);
+		
+		
+		
 		// Create a delay so that we can have it emulate 60Hz/60fps
-		SDL_Delay(16-actual_time_taken);  // 16ms is the time taken for 60Hz and we should also 
-										  // take into account the processing time for the op 
-        
+		SDL_Delay(16);  // 16ms is the time taken for 60Hz and we should also 
+												  // take into account the processing time for the op 
+		update_screen(sdl);
+		
 	}
 	
 	// Clean up SDL
